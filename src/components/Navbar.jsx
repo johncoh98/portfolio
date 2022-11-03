27a5ac68a05b7d19d28686175d75d81c771ab3f6
johncoh98 <John.coh98@gmail.com>
2022-11-03 19:02:11 +0200

    vdd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     const handleClick = () => setnav(!nav);
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300 z-10'>
             <ul className='hidden md:flex'>
                 <li className='hover:text-pink-600'><Link to='home' smooth={true} duration={500}>Home </Link></li>
                 <li className='hover:text-pink-600'><Link to='about' smooth={true} duration={500}>About </Link></li>
@@ -20,22 +20,22 @@ const Navbar = () => {
         <div onClick={handleClick} className='md:hidden z-10'>
            {!nav ?  <FaBars /> : <FaTimes />}
         </div>
-        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-gray-800 flex flex-col justify-center items-center '}>
-            <li className='py-6 text-4xl hover:text-pink-600'><Link to='home' smooth={true} duration={500}>Home</Link></li>
-            <li className='py-6 text-4xl hover:text-pink-600'><Link to='about' smooth={true} duration={500}>About</Link></li>
-            <li className='py-6 text-4xl hover:text-pink-600'><Link to='skills' smooth={true} duration={500}>Skills</Link></li>
-            <li className='py-6 text-4xl hover:text-pink-600'><Link to='projects' smooth={true} duration={500}>Projects</Link></li>
-            <li className='py-6 text-4xl hover:text-pink-600'><Link to='contact' smooth={true} duration={500}>Contact</Link></li>
+        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center z-10'}>
+            <li className='py-6 text-4xl hover:text-pink-600'><Link onClick={handleClick} to='home' smooth={true} duration={500}>Home</Link></li>
+            <li className='py-6 text-4xl hover:text-pink-600'><Link onClick={handleClick} to='about' smooth={true} duration={500}>About</Link></li>
+            <li className='py-6 text-4xl hover:text-pink-600'><Link onClick={handleClick} to='skills' smooth={true} duration={500}>Skills</Link></li>
+            <li className='py-6 text-4xl hover:text-pink-600'><Link onClick={handleClick} to='projects' smooth={true} duration={500}>Projects</Link></li>
+            <li className='py-6 text-4xl hover:text-pink-600'><Link onClick={handleClick} to='contact' smooth={true} duration={500}>Contact</Link></li>
         </ul>
-        <div className='flex fixed flex-col top-[35%] left-0'>
+        <div className='flex fixed flex-col top-[75%] left-0 z-10'>
             <ul>
-                <li className=' w-40 h-[60px] flex justify-between relative md:right-[50%] xs:right-[90%] items-center  mr-[10px] md:hover:ml-[150px] hover:ml-[500px] duration-300 bg-blue-500  '>
+                <li className=' w-40 h-[60px] flex justify-between relative md:right-[50%] xs:right-[60%] items-center  mr-[10px] md:hover:ml-[150px] hover:ml-[500px] duration-300 bg-blue-500  '>
                     <a target="_blank" rel="noreferrer" className='flex justify-between items-center w-full text-gray-300'
                         href="https://www.linkedin.com/in/john-cohen-3233b9248">
                         Linkedin <FaLinkedin size={30}/>
                     </a>
                 </li>
-                <li className=' w-40 h-[60px] flex justify-between relative md:right-[50%] xs:right-[90%] items-center  mr-[10px] md:hover:ml-[150px] hover:ml-[500px] duration-300 bg-gray-500'>
+                <li className=' w-40 h-[60px] flex justify-between relative md:right-[50%] xs:right-[60%] items-center  mr-[10px] md:hover:ml-[150px] hover:ml-[500px] duration-300 bg-gray-500'>
     
                     <a target="_blank" rel="noreferrer" className='flex justify-between items-center w-full text-gray-300'
                         href="https://github.com/johncoh98">
@@ -43,7 +43,7 @@ const Navbar = () => {
                     </a>
                 
                 </li>
-                <li className=' w-40 h-[60px] flex justify-between relative md:right-[50%] xs:right-[90%] items-center  mr-[10px] md:hover:ml-[150px] hover:ml-[500px] duration-300 bg-red-400  '>
+                <li className=' w-40 h-[60px] flex justify-between relative md:right-[50%] xs:right-[60%] items-center  mr-[10px] md:hover:ml-[150px] hover:ml-[500px] duration-300 bg-red-400  '>
                     <a  className='flex justify-between items-center w-full text-gray-300'
                         href="mailto:john.coh98@gmail.com">
                         Mail <FaMailBulk size={30}/>
